fa5fbe0cf5919aba2b1bea2ba59a5173a218f5fd
Aleksandr <rusmakar@mail.ru>
2024-09-26 18:04:55 +0500

    add lesson:task4

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,15 +1,12 @@
 ﻿// Console.WriteLine("Hello, World!");
 
 
-using System.Reflection.PortableExecutable;
-using System.Security.Cryptography;
-
 int value = 100;
 Console.WriteLine(value);
 
 double d = 5.55;
-Char c = '!';
+char c = '!';
 bool b = true;
-Console.Writeline(d);
-Console.Wtiteline(c);
-Console.Writeline(b);
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
